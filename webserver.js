30ba42c921343787c607ccf030162417668ae668
Nəsir Mövlamov <nasir.movlamov@a2z.az>
2022-06-28 23:57:20 +0400

    http webserver from scratch

--- webserver.js
+++ webserver.js
@@ -1,3 +1,27 @@
-const http = require('node:http')
+const tcp = require('node:net');
 
 
+const tcpServer = tcp.createServer(function(socket) {
+    console.log('client connected');
+    
+    socket.on('data', function(data) {
+        console.log('data received: ' + data);
+        const httpRequest = data.toString();
+        const httpResponse = 'HTTP/1.1 200 OK\r\n' +
+            'Content-Type: text/html\r\n' +
+            '\r\n' +
+            '<h1>Hello World</h1>';
+        socket.write(httpResponse);
+        socket.end()
+    });
+    
+    socket.on('end', function() {
+        
+        console.log('client disconnected');
+    });
+});
+
+
+tcpServer.listen(3000, function() {
+    console.log('server bound');
+})
